08b116fd314c80ba6572d7fef320cf3eca50d897
weslyn-cloud <weslyn@lanterncounselling.com.au>
2026-03-25 17:30:18 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Lantern_Counselling_Hub/', 
+  base: '/Lantern_Counselling_Hub/',
   plugins: [react()],
 })
-
-
-
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
