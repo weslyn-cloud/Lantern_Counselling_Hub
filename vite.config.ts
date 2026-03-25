37441ce3e9cb3be07f153458c11fc3e47c7657b3
weslyn-cloud <weslyn@lanterncounselling.com.au>
2026-03-25 17:19:52 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import path from 'path';
+import path from 'Lantern_Counselling_Hub';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+
+
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
